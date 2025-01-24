c1a81c24b95cc8dfce7bc06a2ab6b0660e72702a
Zach Baker <zach@zachbaker.dev>
2025-01-24 11:49:15 -0500

    fix: typo in command

--- deploy/postinstall.sh
+++ deploy/postinstall.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/new-zachbaker.dev || exit 1
 aws --region us-east-2 secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-2:332624953468:secret:prod/zachbaker.dev-wELJx9 --query SecretString --output text > secrets.json
-convert-secrets-env secrets.json --out .env
+convert-json-env secrets.json --out .env
 sudo n 20
 npm ci
 npm run build
