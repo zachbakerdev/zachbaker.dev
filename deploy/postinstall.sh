1e72dc22d5f55a9552e960c17d179a15cb3c87fc
Zach Baker <zach@zachbaker.dev>
2025-01-24 13:30:21 -0500

    feat: update secret id to pull from environment variable

--- deploy/postinstall.sh
+++ deploy/postinstall.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/new-zachbaker.dev || exit 1
-aws --region us-east-2 secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-2:332624953468:secret:prod/zachbaker.dev-wELJx9 --query SecretString --output text > secrets.json
+aws --region us-east-2 secretsmanager get-secret-value --secret-id $PROD_SECRET_ARN --query SecretString --output text > secrets.json
 convert-json-env secrets.json --out .env
 sudo n 20
 npm ci
