6e352ddae33d95965918a455b7d223c0be7c047c
Zach Baker <zach@zachbaker.dev>
2025-01-27 12:43:02 -0500

    feat: signal nginx reload

--- deploy/start.sh
+++ deploy/start.sh
@@ -4,3 +4,4 @@ pm2 start npm --name "zachbaker.dev" -- start
 pm2 save
 sudo cp -fr ./deploy/nginx.conf /etc/nginx/sites-enabled/zachbaker.dev
 sudo chown root /etc/nginx/sites-enabled/zachbaker.dev
+sudo nginx -s reload
