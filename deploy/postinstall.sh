73c15725d0307d688f5680f7bca2ee1a97e66c30
Zach Baker <zach@zachbaker.dev>
2025-01-27 10:31:29 -0500

    fix: own directory before creating secrets.json

--- deploy/postinstall.sh
+++ deploy/postinstall.sh
@@ -1,6 +1,7 @@
 set -e
 source /home/ubuntu/.bashrc
 cd /home/ubuntu/zachbaker.dev || exit 1
+sudo chown -hR ubuntu ./
 echo "$PROD_SECRET_ARN"
 aws --region us-east-2 secretsmanager get-secret-value --secret-id "$PROD_SECRET_ARN" --query SecretString --output text > secrets.json
 convert-json-env secrets.json --out .env
