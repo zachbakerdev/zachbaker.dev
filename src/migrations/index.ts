8cb8173c52978467a8a4523ce47019d9adae8968
Zach Baker <zach@zachbaker.dev>
2025-01-28 08:08:23 -0500

    feat: remove old migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250127_204009 from './20250127_204009';
-
-export const migrations = [
-  {
-    up: migration_20250127_204009.up,
-    down: migration_20250127_204009.down,
-    name: '20250127_204009'
-  },
-];
