b20d619a19e1eb3ac0bc7f76d0d65feb16007ae3
Zach Baker <zach@zachbaker.dev>
2025-01-29 13:47:00 -0500

    feat: add aria label to icon for accessibility

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -32,7 +32,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   return (
     <header className="container relative z-20 " {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-4 flex justify-between">
-        <Link href="/">
+        <Link href="/" aria-label="Home">
           <Logo />
         </Link>
         <HeaderNav data={data} />
